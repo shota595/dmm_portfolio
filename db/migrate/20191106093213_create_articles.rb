98c38308e8928e03bcdc5067eae6bc6fc1828117
shota <ppddccaa165@gmail.com>
2019-11-06 18:44:32 +0900

    made models except user,admin

---
+++ db/migrate/20191106093213_create_articles.rb
@@ -0,0 +1,15 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+      t.text :article_image
+      t.text :article_url
+      t.string :author
+      t.string :published_at
+      t.references :genre, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
