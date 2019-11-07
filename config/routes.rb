764297d70cbfdcc872ef1733b4f92bf6e934de82
shota <ppddccaa165@gmail.com>
2019-11-07 16:11:08 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  get 'categories/create'
+  get 'article_histories/index'
+  get 'searchwords/index'
+  get 'searchwords/create'
+  get 'searchwords/destroy'
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'articles/index'
+  get 'articles/show'
 end
